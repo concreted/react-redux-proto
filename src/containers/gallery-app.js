26a0786f4369aee6122d2cb88da7227cc6066549
Aric Huang <aric.huang@minted.com>
2016-05-14 12:29:20 -0700

    add app skeleton

---
+++ src/containers/gallery-app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+
+const GalleryApp = ({group}) => {
+
+    return (
+      React.createElement('div', {className: 'gallery-app'},
+        "i'm a gallery app"
+      )
+    );
+}
+
+export default GalleryApp;
--- src/server-app.js
+++ src/server-app.js
@@ -4,6 +4,8 @@ import express from 'express';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 
+import GalleryApp from './containers/gallery-app';
+
 // require('babel-polyfill');
 
 const env = process.env.NODE_ENV || 'development';
@@ -11,10 +13,30 @@ let port = process.env.PORT;
 let app = express();
 
 // Built assets.
-app.use('/public', express.static('build'));
+app.use('/public', express.static('src/containers'));
 
 app.get('/', (req, res) => {
   res.status(200).json({ hello: 'world'});
+});
+
+app.get('/gallery/:group', (req, res) => {
+  let {group = ''} = req.params;
+
+  function renderPage() {
+    let renderedApp = renderToString(React.createElement(GalleryApp, {group}));
+
+    let html = `
+      <meta name="viewport" content="width=device-width, initial-scale=0.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0, minimal-ui">
+      <div id="app-container">
+        <h1>${group}</h1>
+        ${renderedApp}
+      </div>
+    `
+
+    return html;
+  }
+
+  res.status(200).send(renderPage());
 });
 
 app.get('/healthcheck', (req, res) => {
